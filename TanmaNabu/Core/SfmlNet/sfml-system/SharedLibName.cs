e2d5f6d4827c9aaf143f903849ce69579c566763
Jakub Kurłowicz <kubagdynia@gmail.com>
2019-04-06 12:35:35 +0200

    Initial SFML project

---
+++ TanmaNabu/Core/SfmlNet/sfml-system/SharedLibName.cs
@@ -0,0 +1,10 @@
+﻿namespace SFML.System
+{
+    public static class CSFML
+    {
+        public const string audio = "csfml-audio-2";
+        public const string graphics = "csfml-graphics-2";
+        public const string system = "csfml-system-2";
+        public const string window = "csfml-window-2";
+    }
+}
